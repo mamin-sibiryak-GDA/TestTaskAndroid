9d49693216083268156a2650f7eaa786d3991229
mamin-sibiryak-GDA <mincrafterdemo300@gmail.com>
2025-02-05 21:11:16 +0300

    Написал апи сервис и сделал все дата классы nullible

--- app/src/main/java/com/example/testtaskandroid/data/ApiClient.kt
+++ app/src/main/java/com/example/testtaskandroid/data/ApiClient.kt
@@ -1,8 +1,25 @@
 package com.example.testtaskandroid.data
 
-class ApiClient {
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object ApiClient {
 
     val httpClient by lazy {
+        OkHttpClient()
+            .newBuilder()
+            .build()
+    }
 
+    val retrofit by lazy {
+        Retrofit.Builder()
+            .client(httpClient)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    val apiService by lazy {
+        retrofit.create(ApiService::class.java)
     }
 }
--- app/src/main/java/com/example/testtaskandroid/data/ApiService.kt
+++ app/src/main/java/com/example/testtaskandroid/data/ApiService.kt
@@ -3,7 +3,7 @@ package com.example.testtaskandroid.data
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("https://drive.usercontent.google.com/u/0/uc?id=1z4TbeDkbfXkvgpoJprXbN85uCcD7f00r&export=download")
+    @GET("https://drive.usercontent.google.com/u/0/uc?id=1z4TbeDkbfXkvgpoJprXbN85uCcD7f00r")
     fun getOffersVacancies(){
 
     }
